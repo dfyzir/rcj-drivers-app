ac33a83dfb83cfb2fddb75549fbd8bed3eeda2ee
Ivan Rusnak <dfyzir@gmail.com>
2023-12-02 14:21:14 -0600

    feat: Add missing configuration for function deployment bucket
    The `team-provider-info.json` file now includes the missing
    configuration for the function deployment bucket. This ensures that the
    correct S3 bucket and key are used during the deployment process.
    
    Resolves #123

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,31 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "awsrcj": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "awsrcje7ee1f46": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "awsrcjlayer50525934": {
+      "Arn": "string"
+    }
+  },
+  "storage": {
+    "s39285eefe": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
