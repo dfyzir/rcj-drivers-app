5f983b928c1e0630d601f66fe5b58697efb496c7
Ivan Rusnak <dfyzir@gmail.com>
2023-12-15 00:44:59 -0600

    Add global metadata tags to _app.tsx for better SEO and mobile responsiveness
    Include <Head> component with title, description, and viewport meta tags
    in order to enhance search engine optimization (SEO) and provide better
    mobile responsiveness for the RCJ Driver application. This will improve
    the visibility and user experience of the application across different
    devices and search engines. No related issues.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,7 @@ import "@aws-amplify/ui-react/styles.css";
 import type { AppProps } from "next/app";
 
 import { NextUIProvider } from "@nextui-org/react";
+import Head from "next/head";
 
 Amplify.configure(config, {
   ssr: true,
@@ -18,6 +19,15 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <Authenticator.Provider>
       <NextUIProvider>
+        <Head>
+          {/* Add other global metadata tags here */}
+          <title>RCJ Driver</title>
+          <meta name="description" content="Find docs for the RCJ's chassis" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+        </Head>
         <Component {...pageProps} />
       </NextUIProvider>
     </Authenticator.Provider>
