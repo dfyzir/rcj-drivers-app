3d8869ecb852ad1b406397e26ed3bb929fbf5418
Ivan Rusnak <dfyzir@gmail.com>
2024-07-06 18:11:53 -0500

    detail modal shows image formats

--- src/components/PDFViewer.tsx
+++ src/components/PDFViewer.tsx
@@ -11,16 +11,25 @@ const PDFViewer = ({ pdfUrl }: { pdfUrl: string }) => {
 
   return (
     <div>
-      <Document
-        file={pdfUrl}
-        className="shadow-2xl rounded-xl overflow-hidden w-[300px] h-[400px]">
-        <Page
-          pageNumber={1}
-          renderTextLayer={false}
-          renderAnnotationLayer={false}
-          className="scale-50 -translate-y-[25%] -translate-x-[25%]"
+      {pdfUrl.includes("pdf") ? (
+        <Document
+          file={pdfUrl}
+          className="shadow-2xl rounded-xl overflow-hidden w-[300px] h-[400px]">
+          <Page
+            pageNumber={1}
+            renderTextLayer={false}
+            renderAnnotationLayer={false}
+            className="scale-50 -translate-y-[25%] -translate-x-[25%]"
+          />
+        </Document>
+      ) : (
+        // eslint-disable-next-line @next/next/no-img-element
+        <img
+          src={pdfUrl}
+          alt="File"
+          className="shadow-2xl rounded-xl overflow-hidden w-[300px] h-[400px] object-cover"
         />
-      </Document>
+      )}
     </div>
   );
 };
